fbaa4e1767e75cdbd6357e2e69b73696adf960b0
George Hanson <gahanson@protonmail.com>
2021-04-18 23:02:14 -0500

    updated model fields with new sizes to reflect url and path character limits

--- app/bookapi/models.py
+++ app/bookapi/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Book(models.Model):
-    local_path = models.CharField(max_length=255, blank=True)
-    remote_url = models.CharField(max_length=500, blank=True)
-    viewer_path = models.CharField(max_length=500, blank=True)
+    local_path = models.CharField(max_length=4096, blank=True)#4096 is the linux file path limit
+    remote_url = models.CharField(max_length=32000, blank=True)#32000 is the firefox url char limit
+    viewer_path = models.CharField(max_length=4096, blank=True)
 
     class Meta:
         ordering = ['local_path']
@@ -13,8 +13,8 @@ class Book(models.Model):
 
 class AvailableBook(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255, blank=True)
-    author = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=1000, blank=True)
+    author = models.CharField(max_length=1000, blank=True)
     
     class Meta:
         ordering = ['title']
@@ -43,8 +43,8 @@ class SingletonModel(models.Model):
 
 class bookapiUserSettings(SingletonModel):
     source_ip = models.CharField(max_length=255, blank=True)
-    source_script_path = models.CharField(max_length=255, blank=True)
-    source_viewer_base_url = models.CharField(max_length=255, blank=True)
+    source_script_path = models.CharField(max_length=4096, blank=True)
+    source_viewer_base_url = models.CharField(max_length=32000, blank=True)
 
     def __str__(self):
         return "bookapiUserSettings"
